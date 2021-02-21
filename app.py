d732a2c42d015336fdae36c2fa9d9952c6a4b6f9
Gabbiiieee <37177883+gabbygab1233@users.noreply.github.com>
2021-02-21 23:47:54 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,125 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+import pickle
+import warnings
+
+
+st.beta_set_page_config(page_title="Heart Failure Prediction", page_icon="💉", layout='centered', initial_sidebar_state='auto')
+
+# Data columns
+feature_names_best = ['age', 'sex', 'anaemia', 'creatinine_phosphokinase', 'diabetes', 'ejection_fraction', 'high_blood_pressure','platelets', 'serum_creatinine', 'serum_sodium', 'smoking', 'time']
+
+gender_dict = {"Male":1,"Female":0}
+feature_dict = {"Yes":1,"No":0}
+
+def load_image(img):
+	im =Image.open(os.path.join(img))
+	return im
+
+def load_model(modelfile):
+	loaded_model = pickle.load(open(modelfile, 'rb'))
+	return loaded_model
+
+def get_value(val,my_dict):
+	for key,value in my_dict.items():
+		if val == key:
+			return value 
+
+def get_fvalue(val):
+	feature_dict = {"Yes":1,"No":0}
+	for key,value in feature_dict.items():
+		if val == key:
+			return value 
+
+# title
+html_temp = """
+<div>
+<h1 style="color:crimson;text-align:left;">Prediction of Survival in Patients with Heart Failure</h1>
+</div>
+"""
+st.markdown(html_temp, unsafe_allow_html=True)
+
+if st.checkbox("Information"):
+	'''
+	Cardiovascular diseases (CVDs) are the number 1 cause of death globally, taking an estimated 17.9 million lives each year, which accounts for 31% of all deaths worlwide.
+	Heart failure is a common event caused by CVDs. Most cardiovascular diseases can be prevented by addressing behavioural risk factors such as tobacco use, unhealthy diet and obesity, physical inactivity and harmful use of alcohol using population-wide strategies.
+	People with cardiovascular disease or who are at high cardiovascular risk (due to the presence of one or more risk factors such as hypertension, diabetes, hyperlipidaemia or already established disease) need early detection and management wherein a machine learning model can be of great help.
+	'''
+'''
+## How does it work ❓ 
+Complete all the questions and the machine learning model will predict the survival of patients with heart failure
+'''
+
+# Logo
+st.sidebar.image('real-heart-icon-png-.png', width=120)
+st.sidebar.title("Prediction Form📋")
+
+# Age of the patient
+age = st.sidebar.number_input("Age", 1,100)
+# Male or Female
+sex = st.sidebar.radio("Sex",tuple(gender_dict.keys()))
+# Decrease of red blood cells or hemoglobin
+anaemia = st.sidebar.radio("Anaemia ?",tuple(feature_dict.keys()))
+# Level of the CPK enzyme in the blood
+creatinine_phosphokinase = st.sidebar.number_input("Creatinine phosphokinase (mcg/L)",1,7861)
+# If the patient has diabetes
+diabetes = st.sidebar.selectbox("Diabetes",tuple(feature_dict.keys()))
+# Percentage of blood leaving
+ejection_fraction = st.sidebar.number_input("Ejection fraction %",1,100)
+# If a patient has hypertension
+high_blood_pressure = st.sidebar.radio("High blood pressure",tuple(feature_dict.keys()))
+# Platelets in the blood
+platelets = st.sidebar.number_input("Platelets (kiloplatelets/mL)",0.0,100000.0)
+# Level of creatinine in the blood
+serum_creatinine = st.sidebar.number_input("Serum creatinine (mg/dL)",0.0,100.0)
+# Level of sodium in the blood
+serum_sodium = st.sidebar.number_input("Serum sodium (mEq/L)",1,1000)
+# If the patient smokes
+smoking = st.sidebar.radio("Smoking",tuple(feature_dict.keys()))
+# Follow-up period
+time = st.sidebar.number_input("Time (follow-up-period)", 1,1000)
+
+feature_list = [age,get_value(sex,gender_dict),get_fvalue(anaemia),creatinine_phosphokinase,get_fvalue(diabetes),ejection_fraction,get_fvalue(high_blood_pressure),platelets,serum_creatinine,serum_sodium,get_fvalue(smoking), time]
+pretty_result = {"Age":age,"Sex":sex,"Anaemia":anaemia,"Creatinine phosphokinase (mcg/L)":creatinine_phosphokinase,"Diabetes":diabetes,"Ejection fraction %":ejection_fraction,"High blood pressure":high_blood_pressure,"Platelets (kiloplatelets/mL)":platelets,"Serum creatinine (mg/dL)":serum_creatinine,"Serum sodium (mEq/L)":serum_sodium,"Smoking":smoking,"Time (follow-up-period)":time}
+'''
+## These are the values you entered 🧑‍⚕
+'''
+st.json(pretty_result)
+single_sample = np.array(feature_list).reshape(1,-1)
+
+if st.button("Predict"):
+		'''
+		## Results 👁‍🗨
+
+		'''
+		loaded_model = load_model('model.pkl')
+		prediction = loaded_model.predict(single_sample)
+		pred_prob = loaded_model.predict_proba(single_sample)
+		
+		if prediction == 1:
+			st.error("The patient will die")
+		else:
+			st.success("The patient will live")
+			
+		for live, die in loaded_model.predict_proba(single_sample):
+				live = f"{live*100:.2f}%"
+				die = f"{die*100:.2f} %"
+				st.table(pd.DataFrame({'Live ':live,
+								'Die': die}, index=['probability']))
+				st.warning("Note: This A.I application is for educational/demo purposes only and cannot be relied upon")
+
+st.sidebar.subheader("Source code")
+st.sidebar.info('''
+
+[![Github](https://i.ibb.co/vDLv9z9/iconfinder-mark-github-298822-3.png)](https://streamlit.io)
+**Github**
+''')    
+
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
---
+++ model.py
@@ -0,0 +1,235 @@
+import pandas as pd
+import pandas_profiling as pp
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+import os
+import plotly.graph_objects as go
+import plotly.io as pio
+import pickle
+from sklearn.utils import resample
+# Metrics
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, auc, roc_curve
+
+# Validation
+from sklearn.model_selection import train_test_split, cross_val_score, KFold
+from sklearn.pipeline import Pipeline, make_pipeline
+
+# Tuning
+from sklearn.model_selection import GridSearchCV
+
+# Feature Extraction
+from sklearn.feature_selection import RFE
+
+# Preprocessing
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, Normalizer, Binarizer, LabelEncoder
+
+# Models
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+# Ensembles
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+
+warnings.filterwarnings('ignore')
+
+
+sns.set_style("whitegrid", {'axes.grid' : False})
+pio.templates.default = "plotly_white"
+
+
+
+df = pd.read_csv('heart_failure_clinical_records_dataset.csv')
+
+
+# Analyze Data
+def explore_data(df):
+    print("Number of Instances and Attributes:", df.shape)
+    print('\n')
+    print('Dataset columns:',df.columns)
+    print('\n')
+    print('Data types of each columns: ', df.info())
+    
+# Checking for duplicates
+def checking_removing_duplicates(df):
+    count_dups = df.duplicated().sum()
+    print("Number of Duplicates: ", count_dups)
+    if count_dups >= 1:
+        df.drop_duplicates(inplace=True)
+        print('Duplicate values removed!')
+    else:
+        print('No Duplicate values')
+    
+
+# Split training and validation set
+def read_in_and_split_data(data, target):
+    X = data.drop(target, axis=1)
+    y = data[target]
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=0)
+    return X_train, X_test, y_train, y_test
+    
+# Spot-Check Algorithms
+def GetModel():
+    Models = []
+    Models.append(('LR'   , LogisticRegression()))
+    Models.append(('LDA'  , LinearDiscriminantAnalysis()))
+    Models.append(('KNN'  , KNeighborsClassifier()))
+    Models.append(('CART' , DecisionTreeClassifier()))
+    Models.append(('NB'   , GaussianNB()))
+    Models.append(('SVM'  , SVC(probability=True)))
+    return Models
+
+def ensemblemodels():
+    ensembles = []
+    ensembles.append(('AB'   , AdaBoostClassifier()))
+    ensembles.append(('GBM'  , GradientBoostingClassifier()))
+    ensembles.append(('RF'   , RandomForestClassifier()))
+    ensembles.append(( 'Bagging' , BaggingClassifier()))
+    ensembles.append(('ET', ExtraTreesClassifier()))
+    return ensembles
+
+# Spot-Check Normalized Models
+def NormalizedModel(nameOfScaler):
+    
+    if nameOfScaler == 'standard':
+        scaler = StandardScaler()
+    elif nameOfScaler =='minmax':
+        scaler = MinMaxScaler()
+    elif nameOfScaler == 'normalizer':
+        scaler = Normalizer()
+    elif nameOfScaler == 'binarizer':
+        scaler = Binarizer()
+
+    pipelines = []
+    pipelines.append((nameOfScaler+'LR'  , Pipeline([('Scaler', scaler),('LR'  , LogisticRegression())])))
+    pipelines.append((nameOfScaler+'LDA' , Pipeline([('Scaler', scaler),('LDA' , LinearDiscriminantAnalysis())])))
+    pipelines.append((nameOfScaler+'KNN' , Pipeline([('Scaler', scaler),('KNN' , KNeighborsClassifier())])))
+    pipelines.append((nameOfScaler+'CART', Pipeline([('Scaler', scaler),('CART', DecisionTreeClassifier())])))
+    pipelines.append((nameOfScaler+'NB'  , Pipeline([('Scaler', scaler),('NB'  , GaussianNB())])))
+    pipelines.append((nameOfScaler+'SVM' , Pipeline([('Scaler', scaler),('SVM' , SVC())])))
+    pipelines.append((nameOfScaler+'AB'  , Pipeline([('Scaler', scaler),('AB'  , AdaBoostClassifier())])  ))
+    pipelines.append((nameOfScaler+'GBM' , Pipeline([('Scaler', scaler),('GMB' , GradientBoostingClassifier())])  ))
+    pipelines.append((nameOfScaler+'RF'  , Pipeline([('Scaler', scaler),('RF'  , RandomForestClassifier())])  ))
+    pipelines.append((nameOfScaler+'ET'  , Pipeline([('Scaler', scaler),('ET'  , ExtraTreesClassifier())])  ))
+
+    return pipelines
+
+# Train model
+def fit_model(X_train, y_train,models):
+    # Test options and evaluation metric
+    num_folds = 10
+    scoring = 'accuracy'
+
+    results = []
+    names = []
+    for name, model in models:
+        kfold = KFold(n_splits=num_folds, random_state=0)
+        cv_results = cross_val_score(model, X_train, y_train, cv=kfold, scoring=scoring)
+        results.append(cv_results)
+        names.append(name)
+        msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+        print(msg)
+        
+    return names, results
+
+# Save trained model
+def save_model(model,filename):
+    pickle.dump(model, open(filename, 'wb'))
+
+# Performance Measure
+def classification_metrics(model, conf_matrix):
+    print(f"Training Accuracy Score: {model.score(X_train, y_train) * 100:.1f}%")
+    print(f"Validation Accuracy Score: {model.score(X_test, y_test) * 100:.1f}%")
+    fig,ax = plt.subplots(figsize=(8,6))
+    sns.heatmap(pd.DataFrame(conf_matrix), annot = True, cmap = 'YlGnBu',fmt = 'g')
+    ax.xaxis.set_label_position('top')
+    plt.tight_layout()
+    plt.title('Confusion matrix for Logisitic Regression Model', fontsize=20, y=1.1)
+    plt.ylabel('Actual label', fontsize=15)
+    plt.xlabel('Predicted label', fontsize=15)
+    plt.show()
+    print(classification_report(y_test, y_pred))
+    
+# ROC_AUC
+def roc_auc(y_test, y_pred):
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred)
+    plt.figure(figsize=(8,6))
+    print(f"roc_auc score: {auc(fpr, tpr)*100:.1f}%")
+    plt.plot(fpr, tpr, color='orange', label='ROC')
+    plt.plot([0, 1], [0, 1], color='darkblue', linestyle='--')
+    plt.xlabel('False Positive Rate',fontsize=12)
+    plt.ylabel('True Positive Rate', fontsize=12)
+    plt.title('Receiver Operating Characteristic (ROC) Curve', fontsize=20)
+    plt.legend()
+    plt.show()
+
+
+Q1 = df.quantile(0.25)
+Q3 = df.quantile(0.75)
+IQR = Q3 - Q1
+
+df_out = df[~((df < (Q1 - 1.5 * IQR)) |(df > (Q3 + 1.5 * IQR))).any(axis=1)]
+
+live = df_out[df_out.DEATH_EVENT == 0]
+die = df_out[df_out.DEATH_EVENT==1]
+
+die_upsampled = resample(die,
+                        replace=True,
+                        n_samples=len(live),
+                        random_state=0)
+
+upsampled = pd.concat([live, die_upsampled])
+
+
+# Split data to training and validation set
+target = 'DEATH_EVENT'
+X_train, X_test, y_train, y_test = read_in_and_split_data(upsampled, target)
+
+#Fit data to model
+models = GetModel()
+names,results = fit_model(X_train, y_train,models)
+
+# Normalize data to improve accuracy
+ScaledModel = NormalizedModel('minmax')
+name,results = fit_model(X_train, y_train, ScaledModel)
+
+# Fine tunong
+
+model = GradientBoostingClassifier()
+n_estimators = [10, 100, 1000]
+learning_rate = [0.001, 0.01, 0.1]
+subsample = [0.5, 0.7, 1.0]
+max_depth = [3, 7, 9]
+#define grid search
+
+grid = dict(learning_rate=learning_rate, n_estimators=n_estimators, subsample=subsample, max_depth=max_depth)
+kfold = KFold(n_splits=10, random_state=0)
+cv_results = cross_val_score(model, X_train, y_train, cv=10, scoring='accuracy')
+grid_search = GridSearchCV(estimator=model, param_grid=grid, n_jobs=-1, cv=10, scoring='accuracy',error_score=0)
+grid_result = grid_search.fit(X_train, y_train)
+# summarize results
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
+# Predict unseen data
+pipeline = make_pipeline(MinMaxScaler(),  GradientBoostingClassifier(learning_rate=0.1, max_depth=9, n_estimators=1000, subsample=0.7))
+model = pipeline.fit(X_train, y_train)
+y_pred = pipeline.predict(X_test)
+conf_matrix = confusion_matrix(y_test,y_pred)
+classification_metrics(pipeline, conf_matrix)
+roc_auc(y_test, y_pred)
+
+save_model(model, 'model.pkl')
